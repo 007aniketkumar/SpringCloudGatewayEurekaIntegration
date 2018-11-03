611c90f5f0b18a8d6dbbf6acdb2ef55178053153
aniket <007aniketkumar@gmail.com>
2018-11-03 15:06:08 +0530

    VERSION 0

---
+++ SpringCloudGatewayEurekaIntegration/src/main/java/com/example/demo/SpringCloudGatewayEurekaIntegrationApplication.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayEurekaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayEurekaIntegrationApplication.class, args);
+	}
+	
+	
+	 /*@Bean
+	    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+	        return builder.routes()
+	                .route(r ->
+	                        r.path("/java/**")
+	                                .filters(
+	                                        f -> f.stripPrefix(1)
+	                                )
+	                                .uri("http://httpbin.org:80")
+	                )
+	                .build();
+	    }*/
+	 
+	 
+	 @Bean
+	    
+	 public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+	        return builder.routes().
+	        		route(r->
+	        		 r.path("/student/**")
+	        		 .and().query("mdexch", "abc")
+	        		 .filters(f-> f.rewritePath("/student/(?<segment>.*)", "/getStudentDetails/$\\{segment}"))
+	        		 
+	        		.uri("lb://student")
+	        		 )
+			.build();
+	        		 
+	                
+	    }
+	 
+	 
+}
